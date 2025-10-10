1105cdda6da98ad98c9b518756e2d3b8a199bff4
Kydyrali <kzantureev@gmail.com>
2025-10-11 04:55:25 +0500

    Fix payments models and admin

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,17 @@
+#Django modules
 from django.contrib import admin
 
-# Register your models here.
+#Project modules
+from .models import Payment, Transaction
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'ride', 'amount', 'payment_method', 'status', 'created_at')
+    list_filter = ('status', 'payment_method')
+    search_fields = ('ride__id',)
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'payment', 'transaction_type', 'amount', 'timestamp')
+    list_filter = ('transaction_type',)
