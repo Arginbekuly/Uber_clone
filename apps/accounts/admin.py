048e4aa0411b60cb4af273eb8127b917df5174ad
Magzhan Dautbekov <magzhandautbekov@gmail.com>
2025-10-14 03:04:45 +0500

    adding mock data for accounts app

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -9,11 +9,8 @@ class AccountAdmin(ModelAdmin):
     list_display = ['email','username','first_name','last_name',
                     'role','is_active'
                     ]
-<<<<<<< HEAD:apps/accounts/admin.py
+
     readonly_fields = ['last_login','date_joined','rating', 'created_at', 'updated_at', 'deleted_at']
-=======
-    readonly_fields = ['last_login','date_joined','rating','created_at','updated_at','deleted_at']
->>>>>>> 065f406179b7d50e717b058711dac654a0ca92d0:accounts/admin.py
     save_on_top = True
     fieldsets = (
         (
