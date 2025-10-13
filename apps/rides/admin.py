23be7bb203bb634cb98dc6d2c8f9b548dfb8b142
Kydyrali <kzantureev@gmail.com>
2025-10-14 02:16:53 +0500

    upd

--- apps/rides/admin.py
+++ apps/rides/admin.py
@@ -5,8 +5,8 @@ from django.contrib import admin
 from .models import Vehicle, Ride
 @admin.register(Vehicle)
 class VehicleAdmin(admin.ModelAdmin):
-    list_display = ('model', 'licence_plate', 'driver', 'capacity')
-    search_fields = ('licence_plate','model', 'driver__username')
+    list_display = ('model', 'license_plate', 'driver', 'capacity')
+    search_fields = ('license_plate','model', 'driver__username')
     
 @admin.register(Ride)
 class RideAdmin(admin.ModelAdmin):
